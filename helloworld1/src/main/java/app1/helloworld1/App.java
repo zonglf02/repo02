ea8382825b2f6b78ccc7cb1883b7ce718f22144e
lzong <linfeng.zong@scotiabank.com>
2018-09-13 18:54:17 -0400

    branchHello1

---
+++ helloworld1/src/main/java/app1/helloworld1/App.java
@@ -0,0 +1,13 @@
+package app1.helloworld1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
